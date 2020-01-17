b172c51a24050a1c1f7d6709c3d2216886ad0bc8
damiecode <damidigi@yahoo.com>
2020-01-17 22:27:13 +0100

    Create Win combinations function

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -1,5 +1,5 @@
 const GameBoard = (() => {
-  let board = ['','','','','','','','',''];
+  let board = ['X','','','','X','','','X',''];
   return { board };
 })();
 
@@ -8,29 +8,30 @@ const PlayerFactory = (name, symbol) => {
 }
 
 const Game = (() => {
-  const winCombinations = ((arr) => {
+  const winCombinations = (arr) => {
+    let result = false;
     [0, 3, 6].forEach(i => {
       if(arr[i] === arr[i+1] && arr[i] === arr[i + 2] && arr[i] !== '') {
-        return true;
+        result = true;
       }
     });
 
     [0, 1, 2].forEach(i => {
       if (arr[i] === arr[i + 3] && arr[i] === arr[i + 6] && arr[i] !== '') {
-        return true;
+        result = true;
       }
     });
 
     if (arr[0] === arr[4] && arr[0] === arr[8] && arr[0] !== '') {
-      return true;
+      result = true;
     }
 
     if (arr[2] === arr[4] && arr[2] === arr[6] && arr[2] !== '') {
-      return true;
+      result = true;
     }
 
-    return false;
-  });
+    return result;
+  };
   return { winCombinations }
-});
+})();
 

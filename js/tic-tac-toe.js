d5d5fd6cba428daeae4842db497ad3613ba1a459
Miguel Prada <mapra99@gmail.com>
2020-01-21 11:27:46 -0500

    Added logic to play a move and to create players

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -10,6 +10,10 @@ const PlayerFactory = (name, symbol) => {
 };
 
 const Game = (() => {
+  let player1 = PlayerFactory('', 'X');
+  let player2 = PlayerFactory('', 'O');
+  let currentPlayer = player1;
+
   const winCombinations = (arr) => {
     let result = false;
     [0, 3, 6].forEach(i => {
@@ -34,8 +38,38 @@ const Game = (() => {
 
     return result;
   };
+  const play = (event) => {
+    let button = event.target;
+    let index = button.getAttribute('data-cell');
+    if (GameBoard.board[index] === '') {
+      GameBoard.board[index] = currentPlayer.symbol;
+      if (currentPlayer === player1) {
+        currentPlayer = player2;
+      } else {
+        currentPlayer = player1;
+      }
+      DisplayController.renderBoard();
+    }
+  };
+  const createPlayers = (event) => {
+    let player1Name = document.getElementById('player1_name').value;
+    let player2Name = document.getElementById('player2_name').value;
 
-  return { winCombinations };
+    player1.name = player1Name;
+    player2.name = player2Name;
+    const form = document.getElementById('players_form');
+    form.reset();
+    form.style.display = "none";
+    DisplayController.renderPlayers();
+  };
+
+  const cells = document.querySelectorAll('.cell');
+  cells.forEach((cell) => cell.addEventListener('click', play));
+
+  const newPlayerbtn = document.querySelector('.addPlayers');
+  newPlayerbtn.addEventListener('click', createPlayers);
+
+  return { winCombinations, player1, player2 };
 })();
 
 const DisplayController = (() => {
@@ -46,6 +80,15 @@ const DisplayController = (() => {
       cell.innerHTML = GameBoard.board[index];
     });
   };
+
+  const renderPlayers = () =>{
+    let divPlayers = document.querySelector('.players');
+    divPlayers.innerHTML = `
+    <p>Player 'X': ${Game.player1.name}</p>
+    <p>Player 'O': ${Game.player2.name}</p>
+    `;
+  };
+
   window.addEventListener('load', renderBoard);
-  return { renderBoard };
+  return { renderBoard, renderPlayers };
 })();

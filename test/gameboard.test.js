7ef170d3688fe793e67f11a0780090136920f697
damiecode <codenlyn@gmail.com>
2020-02-26 19:17:45 +0100

    Add test for board

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,1 +1,6 @@
-const gameboard = require('./tic-tac-toe');
+const GameBoard = require('../src/js/gameboard');
+GameBoard.board = ['', '', '', 'X', '', '', '', '', ''];
+test('should display board', () => {
+    expect(GameBoard.board).toContain('X');
+})
+
---
+++ test/playerFactory.test.js
@@ -0,0 +1,29 @@
+// import PlayerFactory from '../src/js/playerFactory';
+// import Game from '../src/js/play';
+// import DisplayController from '../src/js/displayController';
+
+// describe('player factory', () => {
+//     const player1 = PlayerFactory('', 'X', 0);
+//     const player2 = PlayerFactory('', 'O', 0);
+
+//     const newPlayerbtn = document.querySelector('.addPlayers');
+//   newPlayerbtn.addEventListener('click', createPlayers);
+
+//   const startGame = document.querySelector('.startGame');
+//   startGame.addEventListener('click', () => {
+//     document.getElementById('players_form').style.display = 'block';
+//     startGame.style.display = 'none';
+//   });
+
+//     beforeEach(() => {
+//         player1.createPlayers();
+//         player2.createPlayers();
+//         player1.startGame();
+//         player2.startGame();
+//         player1 = currentPlayer;
+//       });
+     
+//     it('checks if the game starts with player1', () => {
+//         expect(Game.currentPlayer).toBe(player1);
+//     });
+// });
